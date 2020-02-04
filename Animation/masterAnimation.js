1ffaf2bfd3a6c57009da362ecf30afc45b237e7a
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-02-04 18:17:10 -0500

    additional styling

--- Animation/masterAnimation.js
+++ Animation/masterAnimation.js
@@ -4,18 +4,10 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-
-  gsap.from("#sketch1", {
-    duration: 0.7,
-    opacity: 0.1,
-    scale: 0.01,
-    // rotation: 360,
-    x: -1000,
-    y: 0,
-
-
-  });
-
+  var tl = gsap.timeline({ repeat: 2, repeatDelay: 1 });
+  tl.to("#title", { x: 100, duration: 1 });
+  tl.to("#schedule", { y: 50, duration: 1 });
+  tl.to("#addTrain", { opacity: 0, duration: 1 });
 
 
 });  
